79eec1ae2d9ee6bfb5bf6a9770e5671499d299ca
Bobby Sporman <bobby.sporman@yahoo.com>
2020-11-19 17:18:39 -0500

    service-worker updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,47 +1,41 @@
-var CACHE_NAME = "my-site-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-var cachedFiles = [
+const CACHE_NAME = "my-site-cache-v1";
+const dataCache = "data-cache-v1";
+const cachedFiles = [
   "/",
-  "/db.js",
   "/index.js",
   "/manifest.json",
+  "/db.js",
   "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
 
+//install
 self.addEventListener("install", function(event) {
-  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log("Opened cache");
+      console.log("Cached successfully!");
       return cache.addAll(cachedFiles);
     })
   );
 });
 
 self.addEventListener("fetch", function(event) {
-  // cache all get requests to /api routes
   if (event.request.url.includes("/api/")) {
     event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+      caches.open(dataCache).then(cache => {
         return fetch(event.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(event.request.url, response.clone());
             }
-
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
             return cache.match(event.request);
           });
       }).catch(err => console.log(err))
     );
-
     return;
   }
 
@@ -51,7 +45,6 @@ self.addEventListener("fetch", function(event) {
         if (response) {
           return response;
         } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
           return caches.match("/");
         }
       });
